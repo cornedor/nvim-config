d1ad6ad9d016158eca460284b5def20d0e03abbe
Corné Dorrestijn <contact@corne.info>
2023-10-03 22:50:30 +0200

    ?

--- lua/corne/remap.lua
+++ lua/corne/remap.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "Q", "<nop>")
 
+
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
